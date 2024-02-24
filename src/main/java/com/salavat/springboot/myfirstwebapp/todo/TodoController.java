7e5435d3e881d4a6e3ae897b26473fa7ea55cf1c
Salavat <salavat.islamov.96@gmail.com>
2024-02-24 15:14:11 +0100

    Adding new TODO item feature was added

--- src/main/java/com/salavat/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/salavat/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,11 +1,14 @@
 package com.salavat.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -26,4 +29,17 @@ public class TodoController {
 		model.addAttribute("todos", todos);
 		return "listTodos";
 	}
+
+	@RequestMapping(value = "add-todo", method = RequestMethod.GET)
+	public String showNewTodoPage() {
+		return "todo";
+	}
+
+	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
+	public String addNewTodoPage(@RequestParam String description, ModelMap model) {
+		String username = (String) model.get("name");
+		todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+		return "redirect:list-todos";
+	}
+
 }
