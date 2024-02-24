91bd3683e5a58054dd8f99890e7dbb3e4b721df1
Salavat <salavat.islamov.96@gmail.com>
2024-02-24 15:38:59 +0100

    Jakarta Validation added

--- src/main/java/com/salavat/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/salavat/springboot/myfirstwebapp/todo/TodoController.java
@@ -6,9 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("name")
@@ -38,7 +41,10 @@ public class TodoController {
 	}
 
 	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
-	public String addNewTodoPage(ModelMap model, Todo todo) {
+	public String addNewTodoPage(ModelMap model, @Valid Todo todo, BindingResult result) {
+		if (result.hasErrors()) {
+			return "todo";
+		}
 		String username = (String) model.get("name");
 		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
