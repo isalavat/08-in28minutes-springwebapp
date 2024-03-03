2e62bb2ceff32a7475939d3ff7172dd5620d522f
Salavat <salavat.islamov.96@gmail.com>
2024-03-03 15:50:29 +0100

    h2 database with initial data added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'salavat', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'salavat', 'Get AZURE Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'salavat', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'salavat', 'Learn DevOps', CURRENT_DATE(), false);
